5e6aa6b2c727c3067e664692abd07c35d16082d8
Eliane Cristina Kobayakawa <az204-elicrisko@outlook.com>
2025-09-02 16:48:56 +0000

    ajustes

--- 0x01/mei/src/PersonTest.java
+++ 0x01/mei/src/PersonTest.java
@@ -1,3 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class PersonTest {
 
     @BeforeEach
@@ -5,4 +9,25 @@ public class PersonTest {
         Person person = new Person("Paul","McCartney",2000,true,true,true);
     }
 
+    @Test
+    public void show_full_name() {
+        assertEquals("Paul McCartney", person.fullName());
+    }
+
+    @Test 
+    public void test_calculateYearlySalary() {
+        person.setSalary(1200);
+        assertEquals(14400, person.getSalary());
+    }
+
+    @Test
+    public void person_is_MEI(){
+        Person personMei = new Person("Este", "Mei", "02/09/2000", false, false, false);
+        assertTrue(personMei.isMEI());
+    }
+
+    @Test
+    public void person_is_not_MEI() {
+        assertFalse(person.isMEI());
+    }
 }
