f9071037c3d1ab6833f2b51feba8dcacd7370bad
Eliane Cristina Kobayakawa <az204-elicrisko@outlook.com>
2025-10-01 16:54:22 -0300

    ajustes

---
+++ 0x02/password/src/main/java/password/Person.java
@@ -0,0 +1,28 @@
+package password;
+
+public class Person {
+    private String username;
+    private String password;
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Usuário: mínimo 8 caracteres, sem caractere especial
+    public static boolean checkUser(String username) {
+        // Não pode ter caractere especial, só letras e números
+        boolean semEspecial = username.matches("^[A-Za-z0-9]+$");
+        boolean tamanhoMinimo = username.length() >= 8;
+        return semEspecial && tamanhoMinimo;
+    }
+
+    // Senha: mínimo 8 caracteres, 1 maiúscula, 1 número, 1 especial
+    public static boolean checkPassword(String password) {
+        boolean tamanhoMinimo = password.length() >= 8;
+        boolean temMaiuscula = password.matches(".*[A-Z].*");
+        boolean temNumero = password.matches(".*\\d.*");
+        boolean temEspecial = password.matches(".*[^A-Za-z0-9].*");
+        return tamanhoMinimo && temMaiuscula && temNumero && temEspecial;
+    }
+}
